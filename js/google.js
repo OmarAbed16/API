1856fced3ba28ae1f4d2d87fb1509af06067656f
OmarAbed16 <OmarFathiAbed@gmail.com>
2024-10-03 09:32:28 +0300

    update css and js files

--- js/google.js
+++ js/google.js
@@ -9,13 +9,7 @@ function auth_info(a) {
   console.log(a.credential, "2");
   const decodedToken = jwt_decode(a.credential);
   console.log(decodedToken, "3");
-  console.log(
-    decodedToken.name,
-    decodedToken.email,
-    decodedToken.email,
-    decodedToken.email,
-    "4"
-  );
+  console.log(decodedToken.name, decodedToken.email, "4");
 
   logout(decodedToken.email);
 }
--- js/script.js
+++ js/script.js
@@ -15,13 +15,13 @@ signInLink.addEventListener("click", () => {
   wrapper.classList.remove("animated-signin");
 });
 
-function logup(event) {
+function logup(event, us, em, pass, passMatch) {
   event.preventDefault();
 
-  let username = document.getElementById("signup-us").value;
-  let email = document.getElementById("signup-em").value;
-  let password = document.getElementById("signup-pw").value;
-  let passwordMatch = document.getElementById("signup-pwm").value;
+  let username = us;
+  let email = em;
+  let password = pass;
+  let passwordMatch = passMatchdocument.getElementById("signup-pwm").value;
   let signupErrorMessage = document.querySelectorAll(".up-error")[0];
 
   if (password.length < 8) {
@@ -61,11 +61,11 @@ function logup(event) {
   console.log("Signup successful!", newUser);
 }
 
-function login(event) {
+function login(event, em, pw) {
   event.preventDefault();
 
-  let email = document.getElementById("login-em").value;
-  let password = document.getElementById("login-pw").value;
+  let email = em;
+  let password = pw;
   let loginErrorMessage = document.querySelectorAll(".lg-error")[0];
   let users = JSON.parse(localStorage.getItem("users")) || [];
   let user = users.find(
@@ -79,3 +79,20 @@ function login(event) {
     loginErrorMessage.textContent = "Invalid email or password";
   }
 }
+
+const form_lg = document.querySelector(".form-lg");
+let login_email = document.getElementById("login-em").value;
+let login_password = document.getElementById("login-pw").value;
+form_lg.addEventListener("submit", function (event) {
+  login(event, login_email, login_password);
+});
+
+const form = document.querySelector(".form-up");
+
+form.addEventListener("submit", function (event) {
+  let username = document.getElementById("signup-us").value;
+  let email = document.getElementById("signup-em").value;
+  let password = document.getElementById("signup-pw").value;
+  let passwordMatch = document.getElementById("signup-pwm").value;
+  logup(event, username, email, password, passwordMatch);
+});
